1cbe6ac9f64412e63488c8e3589116a6c3fa4502
mihneapopesq <popescumihneagabriel@gmail.com>
2025-01-07 12:30:33 +0200

    continue main

--- main.py
+++ main.py
@@ -19,3 +19,29 @@ class Main(object):
                             constants.Dimensions.APP_MIN_HEIGHT)
         self.__root.maxsize(constants.Dimensions.APP_MAX_WIDTH,
                             constants.Dimensions.APP_MAX_HEIGHT)
+        # todo
+        # self.__root.protocol("WM_DELETE_WINDOW", self.on_exit_button_pressed)
+
+        # Setting MainFrame
+        self.__menu_frame = game_env_interface.MenuFrame(self)
+        self.__arrange_frame = None
+        self.__game_frame = None
+        self.__menu_frame.place_frame()
+        self.__bot = ai.Obj()
+
+        # Setting HelpFrame
+        # todo
+        # self.__help_frame = game_env_interface.HelpFrame(self)
+
+    def start(self):
+        """
+        Starts the mainloop
+        :return: None
+        """
+        self.__root.mainloop()
+
+    def get_root(self):
+        """
+        :return: BaseWidget tkinter root (master)
+        """
+        return self.__root
